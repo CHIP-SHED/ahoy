f0be82429bc09eb3e471186e946ca7a25b52b6a8
computermouth <computermouth@crunchbangplusplus.org>
2016-08-15 16:46:25 -0700

    Fix for licensing header

--- src/ahoy.cpp
+++ src/ahoy.cpp
@@ -1,5 +1,5 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
-and may not be redistributed without written permission.*/
+/*This piece of code was derived from Lazy Foo' Productions SDL image tutorial code
+(http://lazyfoo.net/)*/
 
 #include <fstream>
 #include <iomanip>
@@ -8,51 +8,38 @@ and may not be redistributed without written permission.*/
 #include <stdlib.h>
 using namespace std;
 
-//Using SDL and standard IO
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 
-//Screen dimension constants
 const int SCREEN_WIDTH = 480;
 const int SCREEN_HEIGHT = 272;
 
-//Starts up SDL and creates window
 bool init();
 
-//Loads media
 bool loadMedia();
 
-//Frees media and shuts down SDL
 void close();
 
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
+SDL_Window* nWindow = NULL;
 
-//The surface contained by the window
-SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* nScreenSurface = NULL;
 
-//The image we will load and show on the screen
-SDL_Surface* gImgSurface = NULL;
+SDL_Surface* nImgSurface = NULL;
 
+SDL_AudioDeviceID nAudioDev = 0;
 
-SDL_AudioDeviceID gAudioDev = 0;
-
-//Mouse position structure
-SDL_Point mPosition;
+SDL_Point nPosition;
 
 void null_callback(void* userdata, unsigned char* stream, int len) {
 
 }
 bool init()
 {
-	//Initialization flag
 	bool success = true;
 
-	//Initialize SDL
 	if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_AUDIO ) < 0 )
 	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 		success = false;
 	}
 	else
@@ -66,17 +53,14 @@ bool init()
 		want.samples = 4096;
 		want.callback = null_callback;  // you wrote this function elsewhere.
 
-		gAudioDev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
-		if (gAudioDev == 0) {
-			printf("Failed to open audio: %s\n", SDL_GetError());
+		nAudioDev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
+		if (nAudioDev == 0) {
 			success = false;
 		}
 
-		//Create window
-		gWindow = SDL_CreateWindow( "ahoy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
+		nWindow = SDL_CreateWindow( "ahoy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( nWindow == NULL )
 		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
 			success = false;
 		}
 		else
@@ -84,13 +68,11 @@ bool init()
 			int imgFlags= IMG_INIT_PNG;
 			if( !( IMG_Init(imgFlags) & imgFlags ) )
 			{
-				printf( "SDL image could not be created! SDL_Error: %s\n", IMG_GetError() );
 				success = false;
 			}
 			else
 			{
-				//Get window surface
-				gScreenSurface = SDL_GetWindowSurface( gWindow );
+				nScreenSurface = SDL_GetWindowSurface( nWindow );
 			}
 		}
 	}
@@ -100,14 +82,11 @@ bool init()
 
 bool loadMedia( char* av[], int slide)
 {
-	//Loading success flag
 	bool success = true;
 
-	//Load splash image
-	gImgSurface = IMG_Load(av[slide]);
-	if( gImgSurface == NULL )
+	nImgSurface = IMG_Load(av[slide]);
+	if( nImgSurface == NULL )
 	{
-		printf( "Unable to load image %s! SDL Error: %s\n", av[slide], SDL_GetError() );
 		success = false;
 	}
 
@@ -116,17 +95,14 @@ bool loadMedia( char* av[], int slide)
 
 void close()
 {
-	//Deallocate surface
-	SDL_FreeSurface( gImgSurface );
-	gImgSurface = NULL;
+	SDL_FreeSurface( nImgSurface );
+	nImgSurface = NULL;
 
-	//Destroy window
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
+	SDL_DestroyWindow( nWindow );
+	nWindow = NULL;
 
-	SDL_CloseAudioDevice(gAudioDev);
+	SDL_CloseAudioDevice(nAudioDev);
 
-	//Quit SDL subsystems
 	SDL_Quit();
 }
 
@@ -134,31 +110,25 @@ int main( int argc, char* argv[] )
 {
 	int slide = 2;
 
-	//Start up SDL and create window
 	if( !init() )
 	{
 		printf( "Failed to initialize!\n" );
 	}
 	else
 	{
-		//Load media
 		if( !loadMedia(argv, slide) )
 		{
 			printf( "Failed to load media!\n" );
 		}
 		else
 		{
-			//Main loop flag
 			bool quit = false;
 
-			//Event handler
 			SDL_Event e;
 
-			//While application is running
 			while( !quit )
 			{
-				SDL_GetMouseState( &mPosition.x, &mPosition.y);
-				//Handle events on queue
+				SDL_GetMouseState( &nPosition.x, &nPosition.y);
 				while( SDL_PollEvent( &e ) != 0 )
 				{
 					switch( e.type ) {
@@ -189,11 +159,11 @@ int main( int argc, char* argv[] )
 							}
 							break;
 						case SDL_MOUSEBUTTONUP:
-							if (mPosition.x < 100 && slide > 2){
+							if (nPosition.x < 100 && slide > 2){
 								slide -= 1;
 								if (!loadMedia(argv, slide))
 									quit = true;
-							}else if (mPosition.x > 380){
+							}else if (nPosition.x > 380){
 								if(slide != (argc - 1)){
 									slide += 1;
 									if(!loadMedia(argv, slide)){
@@ -206,16 +176,13 @@ int main( int argc, char* argv[] )
 					}
 				}
 
-				//Apply the image
-				SDL_BlitSurface( gImgSurface, NULL, gScreenSurface, NULL );
-				
-				//Update the surface
-				SDL_UpdateWindowSurface( gWindow );
+				SDL_BlitSurface( nImgSurface, NULL, nScreenSurface, NULL );
+
+				SDL_UpdateWindowSurface( nWindow );
 			}
 		}
 	}
 
-	//Free resources and close SDL
 	close();
 
 	return 0;
